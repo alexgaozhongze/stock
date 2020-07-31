4c6fd84bdce293d0247d72cdde8bd77b374132c6
alex <alexgaozhongze@gmail.com>
2020-07-31 15:49:00 +0800

    go beyond

--- sql/ranking.sql
+++ sql/ranking.sql
@@ -1,7 +1,7 @@
 -- up.ranking
-SELECT `b`.* FROM (
+SELECT `a`.`sup`,`b`.* FROM (
 	SELECT * FROM (
-		SELECT `code`,`type`,SUM(`up`) AS `sup` FROM `hsab` WHERE `date` >= (SELECT MIN(`date`) FROM (SELECT `date` FROM `hsab` WHERE `date` <> CURDATE() GROUP BY `date` ORDER BY `date` DESC LIMIT 18) AS `t`) GROUP BY `code` ORDER BY `sup` DESC
+		SELECT `code`,`type`,SUM(`up`) AS `sup` FROM `hsab` WHERE `date` >= (SELECT MIN(`date`) FROM (SELECT `date` FROM `hsab` WHERE `date` <> CURDATE() GROUP BY `date` ORDER BY `date` DESC LIMIT 18) AS `t`) AND LEFT(`code`,3) NOT IN (300,688) GROUP BY `code` ORDER BY `sup` DESC
 	) AS `a` WHERE `sup` >= 18.99
 ) AS `a` LEFT JOIN `hsab` AS `b` ON `a`.`code`=`b`.`code` AND `b`.`date` >= (SELECT MIN(`date`) FROM (SELECT `date` FROM `hsab` WHERE `date` <> CURDATE() GROUP BY `date` ORDER BY `date` DESC LIMIT 18) AS `t`)
 
